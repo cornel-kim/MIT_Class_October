af4879c2e42f35bddf076b62cf18fa3094d1e56e
cornel-kim <cornelkemboi@yahoo.com>
2020-10-28 08:41:42 +0300

    first commit

--- LESSON1.py
+++ LESSON1.py
@@ -1,6 +1,6 @@
 #web framework- as a backend programming language i.e flask or django
 #data science- due to its rich analytical libraries
 #can be used for android programming due to its object oriented capabilities
-#The adavantages of using python includes security, ease of use, dynamic abilities.
+#The advantages of using python includes security, ease of use, dynamic abilities.
 #Python as a programming language comprises of variables and datatypes. The variables
 # act as information store while the datatypes defines the type of data stored.
