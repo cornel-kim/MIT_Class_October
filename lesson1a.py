98de9434fbb466e49253df4a9fb8515b05a656db
cornel-kim <cornelkemboi@yahoo.com>
2020-10-28 08:52:50 +0300

    first commit

--- lesson1a.py
+++ lesson1a.py
@@ -1,3 +1,11 @@
-#variables-store for information
-student_name = "Jane"#student_name is a variable that store
+# variables-store for information
+student_name = "Jane"  # student_name is a variable that store
 # information name Jane
+# to access information in a variable we use print function
+print(student_name)
+# Python whitespaces, indentation and variable decalarion.
+# variables are written as studentName, student_name- camel case and underscore
+# white spaces always space after assignment operator
+
+subjectName = "English"
+print(subjectName)
