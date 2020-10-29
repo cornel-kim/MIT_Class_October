fd98b5c053a58e2d94319d240e78bc5193666e89
cornel-kim <cornelkemboi@yahoo.com>
2020-10-29 11:46:11 +0300

    first commit

---
+++ lesson2b.py
@@ -0,0 +1,18 @@
+#assignment operators
+a = 5
+b = 8
+#greater than
+c = a > b
+print( c)
+#less than
+d = a < b
+print( d)
+#equals
+e = (a == b)
+print(e)
+#greater or equals to
+f = (a>=b)
+print(f)
+#less or equals to
+g = (a<=b)
+print(g)
