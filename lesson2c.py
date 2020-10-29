73fce68a3786f8ccb91a4719615b9feb1a80b0a6
cornel-kim <cornelkemboi@yahoo.com>
2020-10-29 11:57:29 +0300

    first commit

--- lesson2c.py
+++ lesson2c.py
@@ -4,3 +4,10 @@ kiswahili = 50
 math = 70
 TotalMarks= english + kiswahili + math
 print("The student scored",TotalMarks,"marks")
+
+#simple interest-= (rate * time * amount)/100
+Amount = 3000
+time = 2.5
+rate = 12/100
+simpleInterest = (Amount * 2.5 * 12)/100
+print("Simple interest is",simpleInterest)
