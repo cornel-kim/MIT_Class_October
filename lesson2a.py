69bb4cd08e140209b67c073a3508df7f57118111
cornel-kim <cornelkemboi@yahoo.com>
2020-10-29 11:36:38 +0300

    first commit

---
+++ lesson2a.py
@@ -0,0 +1,19 @@
+#variables
+#datatypes
+#python arithmetic operators: - addition(+), subtraction(-),
+# multiplication(*), divison(/), modulus(%), equals(=)
+#assignment operators: - equals(==), greater than(>),
+# less than(<), greater or equals to (>=), less or equals to(<=)
+x = 10
+y = 6
+addition = x + y
+print("(x + y) =", addition)
+subtraction = x - y
+print("(x - y) = ", subtraction)
+multiplication = x * y
+print("(x * y) = ", multiplication)
+division = x / y
+print("(x / y) = ", division)
+modulus = x % y
+print("(x % y) = ", modulus)
+
