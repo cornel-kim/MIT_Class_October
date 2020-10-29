ba687437a4e14423c5f1a9418f6794d12a55d918
cornel-kim <cornelkemboi@yahoo.com>
2020-10-29 12:10:50 +0300

    first commit

--- lesson2d.py
+++ lesson2d.py
@@ -6,8 +6,8 @@ TotalMarks= english + kiswahili + math
 print("The student scored",TotalMarks,"marks")
 
 #simple interest-= (rate * time * amount)/100
-Amount = 3000
-time = 2.5
-rate = 12/100
-simpleInterest = (Amount * 2.5 * 12)/100
+Amount = int(input("Please input the principle amount:"))
+time = float(input("Please input the period/time:"))
+rate = int(input("Please input the rate:"))/100
+simpleInterest = (Amount * rate * time)/100
 print("Simple interest is",simpleInterest)
