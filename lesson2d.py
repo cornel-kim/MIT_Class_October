d80df478aefb4bfdb0e847b7fce2a7f380681606
cornel-kim <cornelkemboi@yahoo.com>
2020-10-29 12:03:40 +0300

    first commit

---
+++ lesson2d.py
@@ -0,0 +1,13 @@
+#student marks
+english = int(input("Please input your English marks:"))
+kiswahili = int(input("Please input your Kiswahili marks:"))
+math = int(input("Please input your mathemathics marks:"))
+TotalMarks= english + kiswahili + math
+print("The student scored",TotalMarks,"marks")
+
+#simple interest-= (rate * time * amount)/100
+Amount = 3000
+time = 2.5
+rate = 12/100
+simpleInterest = (Amount * 2.5 * 12)/100
+print("Simple interest is",simpleInterest)
