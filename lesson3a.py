dc3df45ed218d5b4838c72a4dc4499238c028cc4
cornel-kim <cornelkemboi@yahoo.com>
2020-11-02 11:22:23 +0300

    first commit

---
+++ lesson3a.py
@@ -0,0 +1,12 @@
+#we have covered, variables, data types and operators.
+#implement python logics. if else statements. conditinal statements
+#mpesa buy airtime- account you are buying for, account balance,
+# pin, okoa jahazi arrears.
+Account = input("Please input the account number:")
+Amount = input("Please input the amount or airtime:")
+Pin = int(input("Input valid pin:"))
+if Pin == 4000:
+    print("You have bought the airtime successfuly")
+
+else:
+    print("wrong pin, try again")
