5dab44d55c33a0c70e9cb3fa0b83dab2eedb8c76
cornel-kim <cornelkemboi@yahoo.com>
2020-11-02 11:24:43 +0300

    second commit

--- lesson3a.py
+++ lesson3a.py
@@ -2,11 +2,18 @@
 #implement python logics. if else statements. conditinal statements
 #mpesa buy airtime- account you are buying for, account balance,
 # pin, okoa jahazi arrears.
-Account = input("Please input the account number:")
-Amount = input("Please input the amount or airtime:")
-Pin = int(input("Input valid pin:"))
-if Pin == 4000:
-    print("You have bought the airtime successfuly")
+# Account = input("Please input the account number:")
+# Amount = input("Please input the amount or airtime:")
+# Pin = int(input("Input valid pin:"))
+# if Pin == 4000:
+#     print("You have bought the airtime successfuly")
+#
+# else:
+#     print("wrong pin, try again")
 
+number = 5#check a number if it is positive or negative
+if number < 0:
+    print("negative number")
 else:
-    print("wrong pin, try again")
+    print("positive number")
+
