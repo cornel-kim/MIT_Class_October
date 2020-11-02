0c65eed926b7987acf3f79a877c343b813786824
cornel-kim <cornelkemboi@yahoo.com>
2020-11-02 12:19:59 +0300

    second commit

--- lessson3c.py
+++ lessson3c.py
@@ -12,3 +12,8 @@ elif student_marks <=50 and student_marks > 0:
     print("Fail")
 else:
     print("input valid marks")
+
+#create a program, i. e an ATM machhine thats checks user pin and card number
+#create a program using student marks to give recomendation for choice of highschool
+#i.e over 380 marks join national, over 320 marks join extra county, over 250 marks
+# join county schools and less than 250 join local
