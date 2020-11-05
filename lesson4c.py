7a6d10bf03f10e2ec99cb9e173c29e82ca11a53e
cornel-kim <cornelkemboi@yahoo.com>
2020-11-05 11:58:24 +0300

    second commit

--- lesson4c.py
+++ lesson4c.py
@@ -1,4 +1,8 @@
-marks = [300, 250, 400, 200, 230, 320]
-x = int
-for x in marks:
-    print(x)
+# marks = [300, 250, 400, 200, 230, 320]
+# x = int
+# for x in marks:
+#     print(x)
+
+students = ["Mary", "Paul", "John", "Mercy", "Susan", "Paul"]
+for student in students:
+    print(student)
