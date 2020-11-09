540ca84d5a7bd1ba3277d0a01fc0f391b9a09384
cornel-kim <cornelkemboi@yahoo.com>
2020-11-09 11:43:09 +0300

    second commit

---
+++ lesson5.py
@@ -0,0 +1,12 @@
+#python functions
+#why we need functions
+#-code reusability.
+#-make programs easier to understand
+#-allow programmers to load a specific part of the function for use
+#-used for object oriented programming
+
+def my_function():#function definition
+    print("MY FUNCTION TODAY")
+
+#invoke/call/trigger/use
+my_function()
