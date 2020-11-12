fdaa88b607e27f73cfca60b314c7f7d846b320a3
cornel-kim <cornelkemboi@yahoo.com>
2020-11-12 08:59:55 +0300

    second commit

--- lesson6c.py
+++ lesson6c.py
@@ -1,15 +1,12 @@
 #an ATM Machine Function
-def ATM():
-    bank_balance = 5000
+def ATM(bank_balance, deposit_amount, withdrawal):
     pin = 2345
-    deposit_amount = 4000
-    withdrawal = 2000
     Total_balance_deposit = bank_balance + deposit_amount
-    Total_balance_withdrawal = bank_balance - withdrawal
+    Total_balance_withdrawal = Total_balance_deposit - withdrawal
     if deposit_amount:
         print("Your total balance is:", Total_balance_deposit)
         if withdrawal < Total_balance_deposit:
-            print("withdrawal is successful")
+            print("withdrawal is successful", "new balance is", Total_balance_withdrawal)
         else:
             print("The balance is not enough")
     elif not deposit_amount:#else if deposit amount == null
@@ -17,9 +14,10 @@ def ATM():
         if withdrawal < Total_balance_deposit:
             print("withdrawal is successful")
         else:
-            print("The balance is not enough")
+            print("The balance is X not enough")
     else:
         print("error please try again")
 
 
-ATM()
+ATM(deposit_amount=3000, withdrawal=1000, bank_balance=4000)
+ATM(deposit_amount=500, withdrawal=5000, bank_balance=4500)
